04e27a3616126b6519072545fc58a61d56a05f54
Clara Veale <vealeclara@gmail.com>
2025-05-29 12:57:13 -0400

    maybe fixed bug

--- scripts.js
+++ scripts.js
@@ -996,7 +996,7 @@ function locos2(){
     console.log(test);
     if(lc != 0 && a3 != 0 && lb != 0){
         la = Math.sqrt(lb* lb + lc * lc -2 * lc * lb * cos(a3));
-        b3 = cosinv(lb * lb - (la * la + lc * lc)/ (-2* la * lc));
+        b3 = cosinv((lb * lb - (la * la + lc * lc)/ (-2* la * lc)));
         c3 = 180 - b3 - a3;
         answer.textContent = "A = " + a3 + " B = " + b3 + " C = " + c3 + " b = " + lb + " c = " + lc + " a = " + la;
         return;  
@@ -1004,7 +1004,7 @@ function locos2(){
 
     if(la != 0 && lc != 0 && b3 != 0){
         lb = Math.sqrt(la* la + lc * lc -2 * la * lc * cos(b3));
-        a3 = cosinv(lc * lc - (la * la + lb * lb)/ (-2* la * lb));
+        a3 = cosinv((lc * lc - (la * la + lb * lb)/ (-2* la * lb)));
         c3 = 180 - b3 - a3;
         answer.textContent = "A = " + a3 + " B = " + b3 + " C = " + c3 + " b = " + lb + " c = " + lc + " a = " + la;
         return;  
@@ -1012,14 +1012,14 @@ function locos2(){
 
         if(lb != 0 && la != 0 && c3 != 0){
         lc = Math.sqrt(la* la + lb * lb -2 * la * lb * cos(c3));
-        a3 = cosinv(lc * lc - (la * la + lb * lb)/ (-2* la * lb));
+        a3 = cosinv((lc * lc - (la * la + lb * lb)/ (-2* la * lb)));
         b3 = 180 - c3 - a3;
         answer.textContent = "A = " + a3 + " B = " + b3 + " C = " + c3 + " b = " + lb + " c = " + lc + " a = " + la;
         return;  
     }
      if(lb != 0 && la != 0 && lc != 0){
-        a3 = cosinv(lc * lc - (la * la + lb * lb)/ (-2* la * lb));
-        b3 = cosinv(lb * lb - (la * la + lc * lc)/ (-2* la * lc));
+        a3 = cosinv((lc * lc - (la * la + lb * lb)/ (-2* la * lb)));
+        b3 = cosinv((lb * lb - (la * la + lc * lc)/ (-2* la * lc)));
         c3 = 180 - b3 - a3;
         answer.textContent = "A = " + a3 + " B = " + b3 + " C = " + c3 + " b = " + lb + " c = " + lc + " a = " + la;
         return;  
